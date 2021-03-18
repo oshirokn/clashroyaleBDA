a3f5fa71d5040a244bdf2b25e6e09d3dced6d505
Ken Oshiro <oshiro.kn@gmail.com>
2021-03-18 13:41:20 +0200

    Update model.py

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 import csv
+from sklearn.svm import SVC
 
 for dirname, _, filenames in os.getcwd():
     for filename in filenames:
@@ -10,11 +11,6 @@ for dirname, _, filenames in os.getcwd():
 
 csv_filename = ".\kaggle\input\clash-royale-season-18-dec-0320-dataset\BattlesStaging_01012021_WL_tagged.csv"
 chunk_size = 500000
-
-cardmasterlist_csv_filename = "/CardMasterListSeason18_12082020.csv"
-df_cardmasterlist = pd.read_csv(cardmasterlist_csv_filename, engine='python',encoding='utf-8-sig', quoting=csv.QUOTE_MINIMAL, index_col = 0)
-df_cardmasterlist.reset_index(inplace=True)
-cardmasterlist_dict = dict(zip(df_cardmasterlist["team.card1.id"],df_cardmasterlist["team.card1.name"]))
 
 text_file_reader = pd.read_csv(csv_filename, engine='python',encoding='utf-8-sig', quoting=csv.QUOTE_MINIMAL, nrows = n_rows, chunksize = chunk_size, index_col=0)
 dfList = []
@@ -33,7 +29,6 @@ columns = ['loser.card1.id', 'loser.card2.id','loser.card3.id', 'loser.card4.id'
 X2 = df[columns]
 X2.columns = ['card1',"card2","card3","card4","card5","card6","card7","card8"]
 X= pd.concat([X, X2], ignore_index=True, sort=True)
-print(X)
 
 columns = ["winner.totalcard.level","winner.troop.count",'winner.structure.count', 'winner.spell.count', 'winner.common.count',
  'winner.rare.count', 'winner.epic.count', 'winner.legendary.count']
@@ -47,7 +42,6 @@ X3.columns= ["totalcard.level","troop.count",'structure.count', 'spell.count', '
  'rare.count', 'epic.count', 'legendary.count']
 X2= pd.concat([X2, X3], ignore_index=True, sort=True)
 X= X.join(X2)
-print(X)
 
 columns = ['winner.trophyChange']
 L = df[columns]
@@ -57,16 +51,9 @@ y2 = pd.DataFrame().reindex_like(L)
 y2 = y2.fillna(0)
 y = pd.concat([y, y2], ignore_index=True, sort=True)
 y.columns = ['result']
-print(y)
-
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-print(y_test.shape)
-print(X_train.shape)
-print(y_train.shape)
-
-from sklearn.neighbors import KNeighborsClassifier
-model = KNeighborsClassifier(n_neighbors=3)
+model = SVC(C=1, gamma=10)
 model.fit(X_train, y_train.values.ravel())
 accuracy = model.score(X_test, y_test)
 print(accuracy)
