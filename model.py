6d40ebfe8c8f437049a9d9e81f55e4ec8133eca7
Ken Oshiro <oshiro.kn@gmail.com>
2021-03-19 18:40:09 +0200

    Update model.py

--- model.py
+++ model.py
@@ -20,6 +20,20 @@ def paths():
     return dr
 dr = paths()
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--C',
+        type=float,
+        default=1,
+    )
+    parser.add_argument(
+        '--gamma',
+        type=float,
+        default=10,
+    )
+    
+parse_args()
 
 dfList = []
 for filename in dr:
@@ -64,7 +78,7 @@ y.columns = ['result']
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-model = SVC(C=1, gamma=10)
+model = SVC(C=C, gamma=gamma)
 model.fit(X_train, y_train.values.ravel())
 accuracy = model.score(X_test, y_test)
 print(accuracy)
