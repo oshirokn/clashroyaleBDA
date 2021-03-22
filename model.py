58868515d7400e61cee5a7b2cc7f64b47fec772a
sergiocristia <63114254+sergiocristia@users.noreply.github.com>
2021-03-22 21:26:46 +0200

    Update model.py

--- model.py
+++ model.py
@@ -97,7 +97,7 @@ def main():
 
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    model = SVC(C=C, gamma=gamma)
+    model = SVC(C=1, gamma=10)
     model.fit(X_train, y_train.values.ravel())
     accuracy = model.score(X_test, y_test)
     # Get the output path from the Valohai machines environment variables
