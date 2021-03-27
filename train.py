ddb2eb6ed3009a6f9034604a0f0cf1f62971ff61
sergiocristia <sergio.cristia_c@sandvik.com>
2021-03-27 16:52:16 +0200

    improv

--- train.py
+++ train.py
@@ -1,9 +1,8 @@
 import os
 
-def test_saved():
-    
+def main():
     path = os.getenv('VH_INPUTS_DIR', './inputs')
     print(os.listdir(path))
 
 if __name__ == '__main__': 
-    test_saved()
+    main()
