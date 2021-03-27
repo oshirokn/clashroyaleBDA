3b61004cbdf9a3e6ef305ca37a38c7f19f6bce1c
sergiocristia <sergio.cristia_c@sandvik.com>
2021-03-27 17:01:36 +0200

    Update train.py

--- train.py
+++ train.py
@@ -3,12 +3,14 @@ import numpy as np
 
 def paths():
     INPUTS_DIR = os.getenv('VH_INPUTS_DIR', './inputs')   
-    path_labels = dr.append(os.path.join(INPUTS_DIR, 'labels/labels.npy'))
+    path_labels = os.path.join(INPUTS_DIR, 'labels/labels.npy')
     return path_labels
 
 def main():
     path_labels = paths()
+    print('Loading data ...')
     y = np.load(path_labels)
+    print('Loading completed.')
     print(y[:10,:])
 
 if __name__ == '__main__': 
