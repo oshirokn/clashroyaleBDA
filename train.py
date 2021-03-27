36386554d6c837883fd72b1a6502fc1ccb89b34f
sergiocristia <sergio.cristia_c@sandvik.com>
2021-03-27 16:59:49 +0200

    improv

--- train.py
+++ train.py
@@ -1,8 +1,16 @@
 import os
+import numpy as np
+
+def paths():
+    INPUTS_DIR = os.getenv('VH_INPUTS_DIR', './inputs')   
+    path_labels = dr.append(os.path.join(INPUTS_DIR, 'labels/labels.npy'))
+    return path_labels
 
 def main():
-    path = os.getenv('VH_INPUTS_DIR', './inputs')
-    print(os.listdir(path))
+    path_labels = paths()
+    y = np.load(path_labels)
+    print(y[:10,:])
 
 if __name__ == '__main__': 
+    paths()
     main()
